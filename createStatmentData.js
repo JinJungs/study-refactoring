879f4e7747f0cbbd87ea18b417165a440b412023
JinJung <67144890+JinJungs@users.noreply.github.com>
2021-09-21 04:48:10 +0900

    희극 공연료 계산코드 옮기기

--- createStatmentData.js
+++ createStatmentData.js
@@ -37,7 +37,14 @@ class TragedyCalculator extends PerformanceCalculator {
     }
 }
 class ComedyCalculator extends PerformanceCalculator {
-
+    get amount() {
+        let result = 30000;
+        if(this.performance.audience > 20){
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
 }
 
 class PerformanceCalculator {
