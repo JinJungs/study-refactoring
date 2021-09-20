0040b48767fa4907cca66080b930e3f74e7b8cf5
JinJung <67144890+JinJungs@users.noreply.github.com>
2021-09-21 04:16:53 +0900

    함수 옮기기
    - 공연료 계산 코드를 계산기 클래스 안으로 복사

--- createStatmentData.js
+++ createStatmentData.js
@@ -21,6 +21,29 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = aPlay;
     }
+
+    get amount() {  // amountFor 함수의 코드를 계산기 클래스로 복사
+        let result = 0;
+        switch(this.play.type) {    // 매개변수로 받던 정보를 계산기 필드에서 바로 얻음
+        case "tragedy" :    // 비극
+            result = 40000;
+            if(this.performance.audience > 30){
+                result += 1000 * (this.performance.audience - 30);
+            }
+            break;
+        case "comedy" :
+            result = 30000;
+            if(this.performance.audience > 20){
+                result += 10000 + 500 * (this.performance.audience - 20);
+            }
+            result += 300 * this.performance.audience;
+            break;
+        default:
+            throw new Error('알 수 없는 경로: ${this.play.type}');
+        }
+    
+        return result;
+    }
 }
 
 
